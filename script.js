a08932b4a874e3b2009f14d81f898b0040ad45e6
Gert <gert.veereste@gmail.com>
2024-02-19 09:14:10 +0200

    waawa fakk ma unustasin commitida nadal aega tagas

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function toggleSections() {
     var sections = document.querySelectorAll('.code-section');
-    sections.forEach(function(section) {
+    sections.forEach(function (section) {
         section.style.display = 'block';
     });
-    
+
     // Hide the dropdown link after it's clicked
     document.getElementById('dropdownLink').style.display = 'none';
 }
 
 function toggleCodeSection(sectionNumber) {
     var sections = document.querySelectorAll('.code-section');
-    sections.forEach(function(section) {
+    sections.forEach(function (section) {
         section.style.display = 'none';
     });
 
@@ -18,9 +18,52 @@ function toggleCodeSection(sectionNumber) {
     if (sectionToShow) {
         sectionToShow.style.display = 'block';
     }
-    
+
     // Hide the button for section 2 after it's clicked
     if (sectionNumber === 2) {
         document.getElementById('section2Button').style.display = 'none';
     }
+}
+
+var draggableElements = document.querySelectorAll(".draggable");
+var droppableElements = document.querySelectorAll(".droppable");
+
+draggableElements.forEach(elem => {
+    elem.addEventListener("dragstart", dragStart);
+});
+
+droppableElements.forEach(elem => {
+    elem.addEventListener("dragenter", dragEnter);
+    elem.addEventListener("dragover", dragOver);
+    elem.addEventListener("dragleave", dragLeave);
+    elem.addEventListener("drop", drop);
+});
+
+function dragStart(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function dragEnter(event) {
+    if (event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+        event.target.classList.add("droppable-hover");
+    }
+}
+
+function dragOver(event) {
+    if (event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+        event.preventDefault();
+    }
+}
+
+function dragLeave(event) {
+    if (event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+        event.target.classList.remove("droppable-hover");
+    }
+}
+
+function drop(event) {
+    event.preventDefault();
+    event.target.classList.remove("droppable-hover");
+    var data = event.dataTransfer.getData('text');
+    event.target.appendChild(document.getElementById(data));
 }
